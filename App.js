9ff3b322b19445dde8a1b3adb8e404b8b30cf7fc
Luis Locon <luis.locon@gmail.com>
2020-11-12 18:49:18 -0600

    First commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>App.Js</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
